62aeeba68f747cf9d852cd0cb2fc587591c56a6c
Hxuu <133578197+hxuu@users.noreply.github.com>
2026-02-18 10:45:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use('/irrigation', irrigationRoutes);
 app.use('/sensors', sensorRoutes);
 app.use('/battery', batteryRoutes);
 
-app.listen(3000, () => {
+app.listen(3002, () => {
     console.log("Server running on port 3000");
 });
